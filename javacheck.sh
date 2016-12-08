fe36100937095ff199df730d7eb2eb2a4e2d2bf1
ebalgza <geza.balogh@ericsson.com>
2016-12-08 11:44:58 +0100

    Just initial testing of repo

---
+++ javacheck.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if type -p java; then
+    echo -e "Found Java executable in \c"
+    type -p java
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "There is no Java installed."
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo "Java version is $version"
+    if [[ "$version" < "1.8" ]]; then
+        echo Java version does not meet the requirements.
+    else
+        echo Java meets the system requirements.
+    fi
+fi
