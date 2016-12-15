540000033a1a460c8c801a5ef359bcd40ee953ca
leftygbalogh <leftygbalogh@gmail.com>
2016-12-15 06:11:22 +0100

    Losta changes, mainly in download script

--- downloadfilefromurl.sh
+++ downloadfilefromurl.sh
@@ -11,5 +11,8 @@ LOGFILE=download.log
 # wget download url
 URL=http://www-us.apache.org/dist/lucene/solr/6.3.0/solr-6.3.0.tgz
 
+#[0-9]+
+
 cd $DIR
 sudo wget $URL -O $FILE -o $LOGFILE
+sudo curl -O $URL --retry 5 -w http_code
