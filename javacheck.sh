48fb1f85eeaa4b5251f624292a8a97007abdb76f
leftygbalogh <leftygbalogh@gmail.com>
2016-12-18 21:38:17 +0100

    intensive testing on Redhat

--- javacheck.sh
+++ javacheck.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
+#_java=$(java -version 2>&1 | head -n 1 | cut -d\" -f 2 | cut -c1-3)
+
 if type -p java; then
-    echo -e "Found Java executable in \c"
-    type -p java
+    echo Found Java.
     _java=java
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo found java executable in JAVA_HOME
+    echo Found Java.     
     _java="$JAVA_HOME/bin/java"
 else
-    echo "There is no Java installed."
+    echo "Java will now be installed"
 fi
+
 
 if [[ "$_java" ]]; then
     version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo "Your current Java version is: $version"
-    if [[ "$version" < "1.8" ]]; then
-        echo Java version does not meet the requirements.
-    else
-        echo Java meets the system requirements.
-    fi
+    echo version "$version"
 fi
+
+    if [[ "$version" > "1.5" ]]; then
+        echo version is more than 1.5
+    else         
+        echo version is less than 1.5
+    fi
