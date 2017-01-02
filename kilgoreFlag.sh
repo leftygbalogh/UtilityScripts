6496880b823f14ee5308fafbcf7eb0277ad9bbef
leftygbalogh@gmail.com <leftygbalogh@gmail.com>
2017-01-02 06:15:18 +0100

    added more flags to list of arguments - still incomplete

--- kilgoreFlag.sh
+++ kilgoreFlag.sh
@@ -1,21 +1,31 @@
 kilgoreFlag=off
 while [ $# -gt 0 ]
+
 do
+    echo number of arguments $#
+
     case "$1" in
         -k | --kilgore)
         kilgoreFlag=on
         echo "Kilgore was here"
-        shift;;
+        ;;
+
         -v | --version)
         SOLRVERSION=$2
         echo "We'll be using Solr $2 as you requested."
-        shift;;
+        ;;
+
+    	-x | --xolp)
+        echo "xauxage"
+        ;;
 
 
 	-h | --help)
-            echo "sausage"
-	    exit 1;;
-	*)  break;;	# terminate while loop
+        echo "sausage"
+	    exit 1
+        ;;
+
+    *)  break;;	# terminate while loop
     esac
     shift
 done
