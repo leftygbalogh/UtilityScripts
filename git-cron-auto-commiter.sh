397dfea57e7b81384265b89996ca34b3cb3b25ed
ebalgza <geza.balogh@ericsson.com>
2017-02-24 15:36:58 +0100

    fixed PATH issue by explicitely calling bash

--- git-cron-auto-commiter.sh
+++ git-cron-auto-commiter.sh
@@ -1,23 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-PATH=/usr/local/git/bin/git:"$PATH"
+#PATH=$PATH:/usr/local/git/bin/
+
 gitFolder=/tmp
 username="Rodimus Prime"
 useremail="autobots@inv.ericsson.commit"
 
+cd $gitFolder
 gitFileList=$(git status | grep "modified: " | cut -d':' -f2 | xargs)
 if [[ $gitFileList == "" ]]; then
+    echo "nothing to do here"
     exit 0
 fi
 
-cd $gitFolder
 git config --global --unset user.name
 git config --global --unset user.email
 git config --global --replace-all user.name "$username"
 git config --global user.email "$useremail"
-
 git add $gitFileList
-git commit -m "automatically commits changes daily"
+git commit -m "$username automatically commits file changes several times a day."
 
 git config --global --unset user.name
 git config --global --unset user.email
