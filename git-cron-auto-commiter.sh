96d6657e2c7f4f102bb6e13d06ab54f7b0e1a49b
ebalgza <geza.balogh@ericsson.com>
2017-02-24 11:19:16 +0100

    finalised autocommiter

--- git-cron-auto-commiter.sh
+++ git-cron-auto-commiter.sh
@@ -2,14 +2,20 @@
 
 PATH=/usr/local/git/bin/git:"$PATH"
 gitFolder=/tmp
-username='Rodimus Prime'
+username="Rodimus Prime"
 useremail="autobots@inv.ericsson.commit"
 
-cd $gitFolder
-git config --global --replace-all user.name $username
-git config --global user.email $useremail
-
 gitFileList=$(git status | grep "modified: " | cut -d':' -f2 | xargs)
+if [[ $gitFileList == "" ]]; then
+    exit 0
+fi
+
+cd $gitFolder
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global --replace-all user.name "$username"
+git config --global user.email "$useremail"
+
 git add $gitFileList
 git commit -m "automatically commits changes daily"
 
