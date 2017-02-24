9ba203f13ecc1ffe511d2d20eed81d20c50f1435
ebalgza <geza.balogh@ericsson.com>
2017-02-24 11:04:27 +0100

    cleaned up file to remove echoing logs

--- git-cron-auto-commiter.sh
+++ git-cron-auto-commiter.sh
@@ -1,32 +1,17 @@
 #!/bin/sh
 
 PATH=/usr/local/git/bin/git:"$PATH"
-
-date
-git status
-echo "========baseline status==============="
-read -n 1 -s
-
 gitFolder=/tmp
 username='Rodimus Prime'
 useremail="autobots@inv.ericsson.commit"
+
 cd $gitFolder
 git config --global --replace-all user.name $username
 git config --global user.email $useremail
 
 gitFileList=$(git status | grep "modified: " | cut -d':' -f2 | xargs)
 git add $gitFileList
-date
-git status
-echo "========staged files==============="
-read -n 1 -s
-
 git commit -m "automatically commits changes daily"
-echo "========commit made==============="
-read -n 1 -s
 
 git config --global --unset user.name
 git config --global --unset user.email
-
-date
-git status
