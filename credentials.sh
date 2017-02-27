cf0db2895a8792fc89f9e01e9838f47679522176
ebalgza <geza.balogh@ericsson.com>
2017-02-27 16:06:00 +0100

    added a few changes

--- credentials.sh
+++ credentials.sh
@@ -2,11 +2,14 @@
 
 export sessionID=$(who am i | cut -d'/' -f2 | cut -d' ' -f1)
 
-dialog --inputbox "Enter your E/// signum or full name:" 8 40 2>/tmp/nameSignumFile$sessionID;
+dialog --title "WARNING" --msgbox "\nYou are accessing a production area." 8 40
 
-dialog --inputbox "Enter your email address:" 8 40 2>/tmp/emailFile$sessionID;
+dialog --inputbox "Enter your name or Ericsson ID:\n" 8 40 2>/tmp/nameSignumFile$sessionID;
 
-clear
+dialog --inputbox "Enter your email address:\n" 8 40 2>/tmp/emailFile$sessionID;
+
+echo ""
 echo "Awesome.  Carry on soldier."
+echo ""
 
-/home/ebalgza/watcher.sh &
+#/home/ebalgza/watcher.sh &
