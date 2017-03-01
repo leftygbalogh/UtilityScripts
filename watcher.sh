c885eb2cd5f7a2fc9a95afbb15e42bd88d5e7d51
leftygbalogh <leftygbalogh@gmail.com>
2017-03-01 17:17:35 +0100

    trying to set the watch on the list of files file

--- watcher.sh
+++ watcher.sh
@@ -11,28 +11,21 @@ filelist="/etc/opt/git-committer/filestowatch.list"
 [ -e $logfile ] || touch $logfile
 
 echo "Git watch is being initiated on $gitWatchDate" >> $logfile
+echo "logfile: $logfile" >> $logfile
+echo "filelist: $filelist" >> $logfile
+echo "=====" >> $logfile
 #all the above is executed only once per run
 
-inotifywait --monitor --quiet --format "%w%f %w %f %e" --outfile $logfile --fromfile $filelist | while read OUTPUT; do
-
+inotifywait --monitor --quiet --format "%f %e" --outfile $logfile --fromfile $filelist | while read OUTPUT; do
 #create log file if it does not exist for the day
-[ -e "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log" ] || touch "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+#[ -e "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log" ] || touch "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
 
+#echo $OUTPUT >> $logfile
 #update date based filename
-logfile="/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
-    echo "macika" >> "$logfile"
-    echo $OUTPUT >> $logfile
-    FILE=$(echo $OUTPUT | cut -d' ' -f1)
-        echo $FILE >> $logfile
+#    echo $OUTPUT >> $logfile
+#    FILE=$(echo $OUTPUT | cut -d' ' -f1)
+#        echo "\nfile: $FILE" >> $logfile
     event=$(echo $OUTPUT | cut -d' ' -f2)
         echo $event >> $logfile
-        echo "$filelist" >> $logfile
-
-
-    if [[ $FILE == "$filelist" ]]; then
-        #statements
-        echo "we need to restart the file if if was changed." >> $logfile
-    fi
-
-
+#        echo "\nfile list: $filelist" >> $logfile
 done
