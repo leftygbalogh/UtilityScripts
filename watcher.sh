09c8bd49a0d61742e1035c2e4c09ea00a434cad7
leftygbalogh <leftygbalogh@gmail.com>
2017-03-01 16:25:40 +0100

    .

--- watcher.sh
+++ watcher.sh
@@ -1,65 +1,38 @@
 #!/bin/sh
-
-/usr/local/git/bin/git init /tmp/
-
+#this script runs indefinitely and MUST be started at reboot
 gitWatchDate=$(date)
 opened="FALSE"
 modified="FALSE"
 closed="FALSE"
+logfile="/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+filelist="/etc/opt/git-committer/filestowatch.list"
 
-echo "Git watch is being initiated on $gitWatchDate"
+#Create the very first log if necessary
+[ -e $logfile ] || touch $logfile
 
-#inotifywait --format "%w%f" --event modify /tmp/ \
-#| while read FILE; do
+echo "Git watch is being initiated on $gitWatchDate" >> $logfile
+#all the above is executed only once per run
 
-#inotifywait --monitor --quiet --format "%w%f %f %w" --event modify /tmp \
-inotifywait --monitor --quiet --format "%w%f %f %w %e" --outfile /home/ebalgza/inotify.log --fromfile /home/ebalgza/inotifyfile.list \
+inotifywait --monitor --quiet --format "%w%f %w %f %e" --outfile $logfile --fromfile $filelist | while read OUTPUT; do
 
-| while read OUTPUT; do
+#create log file if it does not exist for the day
+[ -e "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log" ] || touch "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
 
-    echo $OUTPUT
+#update date based filename
+logfile="/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+    echo "macika" >> "$logfile"
+    echo $OUTPUT >> $logfile
     FILE=$(echo $OUTPUT | cut -d' ' -f1)
-        echo $FILE
-    bareFile=$(echo $OUTPUT | cut -d' ' -f2)
-        echo $bareFile
-    folder=$(echo $OUTPUT | cut -d' ' -f3)
-        echo $folder
-    event=$(echo $OUTPUT | cut -d' ' -f4)
-        echo $event
+        echo $FILE >> $logfile
+    event=$(echo $OUTPUT | cut -d' ' -f2)
+        echo $event >> $logfile
+        echo "$filelist" >> $logfile
 
-#
-#    if [[ $FILE == "/tmp/target.cfg" ]]; then
-#        echo "Running script: $0"
-#        echo "File modified: $FILE"
-#
-#        echo $sessionID " is the session ID"
-#
-#        echo $FILE > /tmp/modifiedFileNameFile$sessionID
-#
-#        export gitName=$(cat /tmp/nameSignumFile$sessionID)
-#        echo $gitName
-#
-#        export gitEmail=$(cat /tmp/emailFile$sessionID)
-#        echo $gitEmail
-#
-#        echo "Adding user to git"
-#        /usr/local/git/bin/git config --global user.email $gitEmail
-#        /usr/local/git/bin/git config --global user.email
-#
-#        /usr/local/git/bin/git config --global user.name $gitName
-#        /usr/local/git/bin/git config --global user.name
-#
-#        echo "Staging and committing $bareFile to git"
-#        cd $folder
-#        /usr/local/git/bin/git add $bareFile
-#        /usr/local/git/bin/git commit $bareFile --message="$gitName ($gitEmail) modified $bareFile in $folder on $(date)"
-#
-#        echo "Removing user from git"
-#        /usr/local/git/bin/git config --global --unset user.email
-#        /usr/local/git/bin/git config --global --unset user.name
-#
-#        /usr/local/git/bin/git config --global user.email
-#        /usr/local/git/bin/git config --global user.name
-#
-#    fi
+
+    if [[ $FILE == "$filelist" ]]; then
+        #statements
+        echo "we need to restart the file if if was changed." >> $logfile
+    fi
+
+
 done
