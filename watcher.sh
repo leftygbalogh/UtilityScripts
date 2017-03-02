7b9789b2865335038ec7484588e9caaea8c6d641
leftygbalogh <leftygbalogh@gmail.com>
2017-03-02 16:30:58 +0100

    finally figured out proper output handling

--- watcher.sh
+++ watcher.sh
@@ -9,23 +9,55 @@ filelist="/etc/opt/git-committer/filestowatch.list"
 
 #Create the very first log if necessary
 [ -e $logfile ] || touch $logfile
+{
+    echo ""
+    echo "+-------------------------------------------+"
+    echo "Git watch is being initiated on $gitWatchDate"
+    echo "Current logfile: $logfile"
+    echo "Current list of watched files is in: $filelist"
+    echo "list of files:"
+    cat $filelist
+    echo ""
+    echo "Currently logged in users:"
+    w -i
+    last | grep "still" | sort
+    echo "+-------------------------------------------+"
+    echo ""
+} >> $logfile
 
-echo "Git watch is being initiated on $gitWatchDate" >> $logfile
-echo "logfile: $logfile" >> $logfile
-echo "filelist: $filelist" >> $logfile
-echo "=====" >> $logfile
 #all the above is executed only once per run
 
-inotifywait --monitor --quiet --format "%f %e" --outfile $logfile --fromfile $filelist | while read OUTPUT; do
-#create log file if it does not exist for the day
-#[ -e "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log" ] || touch "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+inotifywait --monitor --quiet --format "%w%f %e" --fromfile $filelist | while read OUTPUT; do
 
-#echo $OUTPUT >> $logfile
+#create log file if it does not exist for the day
+[ -e "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log" ] || touch "/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+
 #update date based filename
-#    echo $OUTPUT >> $logfile
-#    FILE=$(echo $OUTPUT | cut -d' ' -f1)
-#        echo "\nfile: $FILE" >> $logfile
+logfile="/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+
+{
+    echo $(date) $OUTPUT
+    filename=$(echo $OUTPUT | cut -d' ' -f1)
+    echo $filename
     event=$(echo $OUTPUT | cut -d' ' -f2)
-        echo $event >> $logfile
-#        echo "\nfile list: $filelist" >> $logfile
+    echo $event
+
+    if [ $event == "OPEN" ]; then
+        opened="TRUE"
+    fi
+
+    if [ $event == "MODIFY" ]; then
+        modified="TRUE"
+    fi
+
+    if [ $event == "CLOSE_WRITE,CLOSE" ]; then
+        closed="TRUE"
+    fi
+
+    if [ $opened == "TRUE" ] &&  [ $modified == "TRUE" ] && [ $closed == "TRUE"  ]; then
+        echo "$filename has been changed."
+        echo "Time to call the ghost busters"
+    fi
+
+} >> $logfile
 done
