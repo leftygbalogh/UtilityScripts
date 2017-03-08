3da5a77d87e6ac538ec7d37485e7fc23e5634988
leftygbalogh <leftygbalogh@gmail.com>
2017-03-08 14:42:18 +0100

    using killall to get rid of processes this needs to be refined

--- restarter2.sh
+++ restarter2.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 watcherfile="/var/lib/git-committer/watcher.sh"
+logfile="/var/log/git-committer/file-change-events-$(date +%Y%m%d).log"
+
 #kill the inotifywait process gracefully, it also terminates the old watcher process
-inotifyPID=$1
-kill $inotifyPID
+echo "I am the new restarter script" >> $logfile
+echo "I am killing all watchers and inotify scripts." >> $logfile
+killall inotifywait || killall watcher.sh
+
+#echo $inotifyPID >> $logfile
+
+#inotifyPID=$1
+#kill $inotifyPID
+
 sleep 3
 
 #restart the watcher to pick up the new file list
