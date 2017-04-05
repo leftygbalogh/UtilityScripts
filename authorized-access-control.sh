c667ef3b3b2f6a35bde82d8c0684e0c21b62cf30
leftygbalogh <leftygbalogh@gmail.com>
2017-04-05 16:23:09 +0200

    .

--- authorized-access-control.sh
+++ authorized-access-control.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
-
-
-dialog --inputbox "Enter your ID\n" 8 40 2>"/tmp/nameFile"
-username=$(cat /tmp/nameFile)
+#Ensure that only authorized users can access the SUF and machine processes have free access from  authorized network-IPs
+export sessionID=$(who am i | cut -d'/' -f2 | cut -d' ' -f1)
+mkdir -p "/var/lib/git-committer/"
+nameFile="/var/lib/git-committer/nameSignumFile$sessionID"
 originatingIP=$(echo $SSH_CONNECTION | cut -d' ' -f1)
+authorizedusers=/tmp/authorized-users
 
-if [[ $(cat /tmp/authorized-users | grep -w $username | cut -d':' -f2) = "authorized" ]]; then echo "yes"; else echo "no"; fi
+[ -e "$nameFile" ] || touch "$nameFile"
 
-if [[ $(echo $(cat /tmp/network-IPs | grep -w $(echo $SSH_CONNECTION | cut -d' ' -f1)) | cut -d' ' -f2) = "authorized" ]]; then echo "yes"; else echo "no"; fi
+#Allow network internal addresses to access the SUF
+    if [[ $(cat /tmp/network-IPs | grep -w $originatingIP | cut -d':' -f2) = "internal" ]];
+        then
+            echo "Internal network access. Allow passthrough.";
+            date;
+            echo "Originating IP: $originatingIP";
+
+#Require network external addresses to use an authorized user
+        else
+            echo "Network external address";
+            date;
+            echo "Originating IP: $originatingIP";
+
+            # Collect username
+            dialog --inputbox "Enter your ID\n" 8 40 2>"$nameFile";
+            clear
+
+            username=$(cat "$nameFile")
+
+            #Check if user is authorized to access the system.
+            if [[ $(cat "$authorizedusers" | grep -i -w $username | cut -d':' -f2) = "authorized" ]];
+
+            then
+                echo "Welcome home, commander $username.";
+
+            else
+                clear;
+                echo "We've got a bogie at $(date +"%H") o'clock. Bogie arrived from $originatingIP.";
+                echo $(exit -1);
+
+            fi
+    fi
